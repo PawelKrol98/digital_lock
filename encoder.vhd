40e761c09b0d59d7d9591b0aeba11e70c4500584
PawelKrol98 <46727380+PawelKrol98@users.noreply.github.com>
2021-09-06 21:07:53 +0200

    Feature - changing password V1

---
+++ encoder.vhd
@@ -0,0 +1,95 @@
+-------------------------------------------------------------------------------
+--
+-- Title       : encoder
+-- Design      : digital_lock
+-- Author      : Pawe³ Król
+-- Company     : AGH UST	  
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity encoder is
+	 port(
+		 R : in STD_LOGIC_VECTOR(0 to 3);
+		 C : buffer STD_LOGIC_VECTOR(0 to 3) := "0000";
+		 CLK : in STD_LOGIC;
+		 PUSH : out STD_LOGIC := '0';
+		 NUMBER : out STD_LOGIC_VECTOR(0 to 3)
+	     );
+end encoder;
+
+
+architecture encoder of encoder is			 
+begin
+	process(CLK)
+	variable count : INTEGER RANGE 0 TO 100 := 0;
+	begin
+		if rising_edge(CLK) then
+			if(count < 100) then
+				count := count +1;
+			else
+				count := 0;
+				if C = "0000" then
+					if R = "1111" then
+						PUSH <= '0';
+					end if;
+					C <= "0111";
+				elsif C = "0111" then
+					if R = "0111" then
+					elsif R = "1011" then
+					elsif R = "1101" then
+					elsif R = "1110" then
+						NUMBER <= "1010";
+						PUSH <= '1';
+					end if;
+					C <= "1011";
+				elsif C	= "1011" then
+					if R = "0111" then
+						NUMBER <= "1110";
+						PUSH <= '1';
+					elsif R = "1011" then
+						NUMBER <= "1001";
+						PUSH <= '1';
+					elsif R = "1101" then
+						NUMBER <= "0110";
+						PUSH <= '1';
+					elsif R = "1110" then
+						NUMBER <= "0011";
+						PUSH<= '1';
+					end if;
+					C <= "1101";
+				elsif C	= "1101" then
+					if R = "0111" then
+						NUMBER <= "1111";
+						PUSH <= '1';
+					elsif R = "1011" then
+						NUMBER <= "1000";
+						PUSH <= '1';
+					elsif R = "1101" then
+						NUMBER <= "0101";
+						PUSH <= '1';
+					elsif R = "1110" then
+						NUMBER <= "0010";
+						PUSH <= '1';
+					end if;
+					C <= "1110";
+				elsif C = "1110" then
+					if R = "0111" then
+						NUMBER <= "0000";
+						PUSH <= '1';
+					elsif R = "1011" then
+						NUMBER <= "0111";
+						PUSH <= '1';
+					elsif R = "1101" then
+						NUMBER <= "0100";
+						PUSH <= '1';
+					elsif R = "1110" then
+						NUMBER <= "0001";
+						PUSH <= '1';
+					end if;
+					C <= "0000";
+				end if;
+			end if;
+		end if;
+	end process;
+end encoder;
--- lock.vhd
+++ lock.vhd
@@ -21,71 +21,117 @@ end lock;
 												  
 
 architecture lock of lock is
-	signal PASSWORD: STD_LOGIC_VECTOR(0 to 15) := "0000000000000000";		 
-	signal STATE: STD_LOGIC_VECTOR(0 to 2) := "000";
+	signal PASSWORD: STD_LOGIC_VECTOR(0 to 15) := "0000000000000000";
+	signal NEW_PASSWORD: STD_LOGIC_VECTOR(0 to 15) := "0000000000000000";
+	signal STATE: STD_LOGIC_VECTOR(0 to 4) := "00000";
 	-- state 0 - ready to enter password
-	-- state 1 - 1 digits entered
+	-- state 1 - 1 digit entered
 	-- state 2 - 2 digits entered
 	-- state 3 - 3 digits entered
 	-- state 4 - 4 digits entered and password not checked
 	-- state 5 - password checked, door opened
 	-- state 6 - password checked, door closed
-	constant CORRECT_PASSWORD: STD_LOGIC_VECTOR(0 to 15) := "0010000100110110";
+	-- state 7 - changing password, entering actual password, 0 digit entered
+	-- state 8 - changing password, entering actual password, 1 digits entered
+	-- state 9 - changing password, entering actual password, 2 digits entered
+	-- state 10 - changing password, entering actual password, 3 digits entered
+	-- state 11 - changing password, entering actual password, 4 digits entered
+	-- state 12 - changing password, entering new password, 0 digits entered
+	-- state 13 - changing password, entering new password, 1 digits entered
+	-- state 14 - changing password, entering new password, 2 digits entered
+	-- state 15 - changing password, entering new password, 3 digits entered
+	-- state 16 - changing password, entering new password,  digits entered
+	signal CORRECT_PASSWORD: STD_LOGIC_VECTOR(0 to 15) := "0010000100110110";
 begin
 	process (PUSH)
 	begin
 		if rising_edge(PUSH) then
 			if NUMBER <= 9 then
-				if STATE = 0 then
+				if STATE = 0 or STATE = 7 then
 					PASSWORD(0 to 3) <=  NUMBER;
 					STATE <= STATE + 1;
-				elsif STATE = 1 then 
+				elsif STATE = 1 or STATE = 8 then 
 					PASSWORD(4 to 7) <= NUMBER;
 					STATE <= STATE + 1;
-				elsif STATE = 2 then 
+				elsif STATE = 2 or STATE = 9 then 
 					PASSWORD(8 to 11) <= NUMBER;
 					STATE <= STATE + 1;
-				elsif STATE = 3 then 
+				elsif STATE = 3 or STATE = 10 then 
 					PASSWORD(12 to 15) <= NUMBER;
+					STATE <= STATE + 1;
+				elsif STATE = 12 then
+					NEW_PASSWORD(0 to 3) <=  NUMBER;
+					STATE <= STATE + 1;
+				elsif STATE = 13 then 
+					NEW_PASSWORD(4 to 7) <= NUMBER;
+					STATE <= STATE + 1;
+				elsif STATE = 14 then 
+					NEW_PASSWORD(8 to 11) <= NUMBER;
+					STATE <= STATE + 1;
+				elsif STATE = 15 then 
+					NEW_PASSWORD(12 to 15) <= NUMBER;
 					STATE <= STATE + 1;
 				end if;
 			elsif NUMBER = 15 then
-				if STATE = 1 then
+				if STATE = 1 or STATE = 8 then
 					PASSWORD(0 to 3) <=  "0000";
 					STATE <= STATE - 1;
-				elsif STATE = 2 then 
+				elsif STATE = 2 or STATE = 9 then 
 					PASSWORD(4 to 7) <= "0000";
 					STATE <= STATE - 1;
-				elsif STATE = 3 then 
+				elsif STATE = 3 or STATE = 10 then 
 					PASSWORD(8 to 11) <= "0000";
 					STATE <= STATE - 1;
-				elsif STATE = 4 then 
+				elsif STATE = 4 or STATE = 11 then 
 					PASSWORD(12 to 15) <= "0000";
+					STATE <= STATE - 1;
+				elsif STATE = 13 then
+					NEW_PASSWORD(0 to 3) <=  "0000";
+					STATE <= STATE - 1;
+				elsif STATE = 14 then 
+					NEW_PASSWORD(4 to 7) <= "0000";
+					STATE <= STATE - 1;
+				elsif STATE = 15 then 
+					NEW_PASSWORD(8 to 11) <= "0000";
+					STATE <= STATE - 1;
+				elsif STATE = 16 then 
+					NEW_PASSWORD(12 to 15) <= "0000";
 					STATE <= STATE - 1;
 				end if;
 			elsif NUMBER = 14 then
-				if STATE = 4 then
+				if STATE = 4 or STATE = 11then
 					if CORRECT_PASSWORD = PASSWORD then
-						STATE <= "101";
+						if STATE = 11 then 
+							STATE <= STATE + 1;
+						else
+							STATE <= "00101";
+						end if;
 					else
-						STATE <= "110";
+						STATE <= "00110";
 					end if;
 				elsif STATE = 5 or STATE = 6 then
-					STATE <= "000";
+					STATE <= "00000";
 					PASSWORD <= "0000000000000000";
+				elsif STATE = 16 then
+					STATE <= "00000";
+					CORRECT_PASSWORD <= NEW_PASSWORD;
+				end if;
+			elsif NUMBER = 10 then
+				if STATE = 0 then
+					STATE <= "00111";
 				end if;
 			end if;
 		end if;
 	end process;
 	
-	TYPED <= "1000" when STATE = "001" else
-			 "1100" when STATE = "010" else
-			 "1110" when STATE = "011" else
-			 "1111" when STATE = "100" else
+	TYPED <= "1000" when STATE = 1 or STATE = 8 or STATE = 13 else
+			 "1100" when STATE = 2 or STATE = 9 or STATE = 14 else
+			 "1110" when STATE = 3 or STATE = 10 or STATE = 15 else
+			 "1111" when STATE = 4 or STATE = 11 or STATE = 16 else
 			 "0000";
-	OPEN_DOOR <= '1' when STATE = "101" else
+	OPEN_DOOR <= '1' when STATE = "00101" or STATE >= 7 else
 			  	 '0';
-	CLOSE_DOOR <= '1' when STATE = "110" else
+	CLOSE_DOOR <= '1' when STATE >= 6 else
 				  '0';
 	
 end lock;
